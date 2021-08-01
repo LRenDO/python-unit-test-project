c7c8991d3eefeebbdd22562af1e22b8452c5a1b0
Ren Demeis-Ortiz <ldemeisortiz@gmail.com>
2021-08-01 16:59:55 -0400

    Added conv_hex and tests. Passed tests.
    Also added calc_complement helper function. Assumed -0x0 should return None. Need to clarify if it should return
    None or 0.

--- task.py
+++ task.py
@@ -13,7 +13,11 @@ def conv_num(num_string):
         decimal or hex (must be in 0x or -0x prefix format)
     Returns: integer or float
     """
-    if num_string.find('.') != -1:
+    if num_string[:2] == '0x' or num_string[:3] == '-0x':
+        num_string = num_string.upper()
+        return conv_hex(num_string)
+
+    elif num_string.find('.') != -1:
         return conv_float(num_string)
 
     return conv_int(num_string)
@@ -58,3 +62,60 @@ def conv_float(num_string):
         right_str = num_string[decimal_index + 1:len(num_string)]
         right_side = conv_int(right_str) * pow(10, -len(right_str))
         return left_side + right_side
+
+
+def conv_hex(num_string):
+    """
+    Converts strings of hex numbers with prefix 0x into base 10 integer
+
+    Parameter: num_string (string) number to be converted
+    Returns: base 10 integer
+    """
+    if num_string[0] == '-':
+        if num_string == '-0X0':
+            return
+        else:
+            num_string = calc_complement(num_string[3:])
+            return - conv_hex(num_string)
+
+    if num_string[:2] == '0X':
+        num_string = num_string[2:]
+
+    num = 0
+    exp = 0
+    for char in reversed(num_string):
+        if char.isupper():
+            digit = ord(char) - ord('A') + 10
+        else:
+            digit = ord(char) - ord('0')
+        num = num + digit * pow(16, exp)
+        exp += 1
+    return num
+
+
+def calc_complement(num_string):
+    """
+    Returns two's complement of hex number converted
+
+    Parameter: num_string (string) hex to get two's complement of. All letters
+        must be capitalized.
+    Returns: string that is the inverse of the original hex
+    """
+    complement = ''
+    is_last_digit = True
+    for char in reversed(num_string):
+        if char.isupper():
+            comp_digit = 15 - (ord(char) - ord('A') + 10)
+        else:
+            comp_digit = 15 - (ord(char) - ord('0'))
+
+        if is_last_digit:
+            comp_digit += 1
+            is_last_digit = False
+
+        if comp_digit >= 10:
+            comp_digit = chr(comp_digit + ord('A') - 10)
+
+        complement = str(comp_digit) + complement
+
+    return complement
--- tests.py
+++ tests.py
@@ -49,6 +49,34 @@ class TestCase(unittest.TestCase):
         self.assertEqual(task.conv_num(test_case), expected,
                          msg='conv_num({}) Failed'.format(expected))
 
+    # Tests hex conversion to base 10 0x0
+    def test_conv_num_hex_0(self):
+        test_case = '0x0'
+        expected = 0
+        self.assertEqual(task.conv_num(test_case), expected,
+                         msg='conv_num({}) Failed'.format(expected))
+
+    # Tests hex conversion to base 10 0x0
+    def test_conv_num_hex_neg0(self):
+        test_case = '-0x0'
+        expected = None
+        self.assertEqual(task.conv_num(test_case), expected,
+                         msg='conv_num({}) Failed'.format(expected))
+
+    # Tests hex conversion to base 10 0xAD4
+    def test_conv_num_hex_pos(self):
+        test_case = '0xAD4'
+        expected = 2772
+        self.assertEqual(task.conv_num(test_case), expected,
+                         msg='conv_num({}) Failed'.format(expected))
+
+    # Tests hex conversion to base 10 -0xF52C
+    def test_conv_num_hex_neg(self):
+        test_case = '-0xF52C'
+        expected = -2772
+        self.assertEqual(task.conv_num(test_case), expected,
+                         msg='conv_num({}) Failed'.format(expected))
+
 
 if __name__ == '__main__':
     unittest.main()
