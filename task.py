4874b4c001b3520d653d9bc5a6df351120ad0fd4
adamslou <adamslou@oregonstate.edu>
2021-08-01 16:38:59 -0500

    Add initial tests and made conv_endian functional for big endian

--- task.py
+++ task.py
@@ -2,17 +2,19 @@
 # CS362 Summer 2021
 # Due Date: 2021-08-09
 
+
 def conv_endian(num, endian='big'):
-    """This function converts a decimal integer to a string of hexadecimal 
+    """This function converts a decimal integer to a string of hexadecimal
     bytes
 
     :param num: a decimal integer to be converted to hexadecimal bytes
-    :param endian: can be 'big' or 'little', 'big' is the default if not 
+    :param endian: can be 'big' or 'little', 'big' is the default if not
                    supplied
     :return: a string of hexadecimal bytes in big or little endian form
              separated by spaces, may include a negative sign
 
-    adapted from: https://pencilprogrammer.com/python-programs/convert-decimal-to-hexadecimal/
+    adapted from:
+    https://pencilprogrammer.com/python-programs/convert-decimal-to-hexadecimal
     """
     if endian != 'big' and endian != 'little':
         return None
@@ -24,19 +26,22 @@ def conv_endian(num, endian='big'):
                   'C', 'D', 'E', 'F']
     decimal = abs(num)
     hex_str = ''
-    digit_num = 0       # each byte is made up of 2 hex digits, digit_num
-                        # keeps track of which digit we are currently on
+
+    # each byte is made up of 2 hex digits, digit_num keeps track of which
+    # digit we are currently on
+    digit_num = 0
+
     while(decimal > 0):
         remainder = decimal % 16
         hex_str = hex_digits[remainder] + hex_str
         digit_num += 1
         decimal = decimal // 16
-        if digit_num == 2 and decimal != 0: # add a space only if we have
-            hex_str = ' ' + hex_str         # finished a byte and have more
-            digit_num = 0                   # bytes to write
+        if digit_num == 2 and decimal != 0:     # add a space only if we have
+            hex_str = ' ' + hex_str             # finished a byte and have more
+            digit_num = 0                       # bytes to write
 
-    if digit_num == 1:                      # fill out the final byte with an
-        hex_str = '0' + hex_str             # initial zero if necessary
+    if digit_num == 1:                          # fill out the final byte with
+        hex_str = '0' + hex_str                 # an initial zero if necessary
 
     if num < 0:
         hex_str = '-' + hex_str
